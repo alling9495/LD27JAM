c10aed24dfe060a89050728c1286f15f888669a0
kyle-piddington <kpiddy@gmail.com>
2013-08-23 22:37:30 -0700

    Added accessor and basic framework to tubeMessage

--- LD27JAM_Project/Assets/Scripts/tubeMessage.cs
+++ LD27JAM_Project/Assets/Scripts/tubeMessage.cs
@@ -1,13 +1,52 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class tubeMessage : MonoBehaviour {
 
-	// Use this for initialization
+    public enum messageColor { red, green, orange, blue, rg, go, ob, br } // Colors avalible to the tubes. 
+    private List<messageColor> colorList = new List<messageColor>();
+    public int deskNum;
+    private int stamps = 0;
+    // Use this for initialization
+
 	void Start () {
-	
+	    
 	}
-	
+
+    public void setColors(List<messageColor> colors)
+    {
+        foreach (messageColor c in colors)
+        {
+            colorList.Add(c);
+        }
+    }
+
+    public messageColor currUnstampedColor()
+    {
+        if (stamps >= colorList.Count-1)
+        {
+            return colorList[colorList.Count - 1];
+        }
+        else
+        {
+            return colorList[stamps];
+        }
+    }
+    public bool completed()
+    {
+        return stamps == colorList.Count;
+    }
+
+    public void stamp()
+    {
+        stamps++;
+    }
+
+    public int getCurrStamps()
+    {
+        return stamps;
+    }
+
 	// Update is called once per frame
 	void Update () {
 	
